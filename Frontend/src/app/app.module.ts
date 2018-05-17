d13ed4c7c65446566fdf34ba18b62b68738c9b00
Matías Hermosilla <matiasahermosilla@gmail.com>
2018-05-16 22:00:16 -0400

    Contenido base

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -1,14 +1,47 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+
+// Material
+import {
+  MatButtonModule,
+  MatCheckboxModule,
+  MatSidenavModule,
+  MatMenuModule,
+  MatIconModule,
+  MatCardModule,
+  MatToolbarModule
+} from '@angular/material';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+
+    // Material
+    MatButtonModule,
+    MatCheckboxModule,
+    MatSidenavModule,
+    MatMenuModule,
+    MatIconModule,
+    MatCardModule,
+    MatToolbarModule
+  ],
+  exports: [
+
+    // Material
+    MatButtonModule,
+    MatCheckboxModule,
+    MatSidenavModule,
+    MatMenuModule,
+    MatIconModule,
+    MatCardModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
