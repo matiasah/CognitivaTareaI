c7c04728eed4bed3472e39c01cd11fabaf9cfcf8
Matías Hermosilla <matiasahermosilla@gmail.com>
2018-05-18 22:59:33 -0400

    Agregando vistas

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -7,12 +7,14 @@ import { routing, appRoutingProviders } from './app.routing';
 import { RootComponent } from './components/root/root.component';
 import { MaterializeModule } from 'angular2-materialize';
 import { ProcesarTextoComponent } from './components/procesartexto/procesartexto.component';
+import { ProcesarArchivoComponent } from './components/procesararchivo/procesararchivo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     RootComponent,
-    ProcesarTextoComponent
+    ProcesarTextoComponent,
+    ProcesarArchivoComponent
   ],
   imports: [
     BrowserModule,
---
+++ Frontend/src/app/components/procesararchivo/procesararchivo.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'procesararchivo',
+    templateUrl: 'procesararchivo.component.html'
+})
+export class ProcesarArchivoComponent {
+
+    public constructor() {
+        
+    }
+
+}
